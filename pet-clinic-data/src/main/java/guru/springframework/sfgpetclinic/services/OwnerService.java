c0d0a2c02714ae77d5a0d27558f046accc123263
Harry S. Green <harrysgreen@Harrys-MBP.fios-router.home>
2022-09-21 22:17:47 -0400

    Modified service interfaces for Owner, Pet, Vet

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -1,9 +1,8 @@
 package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Owner;
-
 import java.util.List;
-
+import java.util.Set;
 
 /**
  * Created by jt on 7/18/18.
@@ -13,4 +12,10 @@ public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
 
     List<Owner> findAllByLastNameLike(String lastName);
+    
+    Owner findById(Long Id);
+    
+    Owner save(Owner owner);
+    
+    Set<Owner> findAll();
  }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
@@ -1,11 +1,16 @@
-package guru.springframework.sfgpetclinic.services;
+package guru.springframework.sfgVetclinic.services;
 
-import guru.springframework.sfgpetclinic.model.Vet;
-
+import guru.springframework.sfgVetclinic.model.Vet;
+import java.util.Set;
 
 /**
  * Created by jt on 7/18/18.
  */
 public interface VetService extends CrudService<Vet, Long>{
 
+    Vet findById(Long Id);
+    
+    Vet save(Vet vet);
+    
+    Set<Vet> findAll();
 }
